04e6e893f27e05b8809c8ccdf0316210f4cd349b
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-21 18:56:06 +0100

    FEAT bonus testo rigato

--- js/app.js
+++ js/app.js
@@ -10,23 +10,22 @@ createApp({
   },
   methods: {
     addTask() {
-        const newTask = {
-            text: this.inputValue,
-            done: false,
-            showCloseIcon: false
-        }
-        this.tasks.push(newTask)
-        this.inputValue = ''
+      const newTask = {
+        text: this.inputValue,
+        done: false,
+        showCloseIcon: false,
+        showDoneIcon: false,
+      };
+      this.tasks.push(newTask);
+      this.inputValue = "";
     },
-    
-
+  
+    toggleDone(index) {
+      this.tasks[index].done = !this.tasks[index].done;
+    },
+  
     removeTask(startIndex) {
-      console.log(
-        "elimino il task con indice:",
-        startIndex,
-        this.tasks[startIndex]
-      );
       this.tasks.splice(startIndex, 1);
     },
-  },
+  }
 }).mount("#app");
